b89c07ff22fe7cc35a75b0e4c8528a989e9377e9
ToyonPROFIT <training.skill@bk.ru>
2023-01-04 01:54:55 +0900

    задачки на списки

--- stepik.py
+++ stepik.py
@@ -69,7 +69,50 @@
 #print(f'Аденин: {s.count("а")}\nГуанин: {s.count("г")}\nЦитозин: {s.count("ц")}\nТимин: {s.count("т")}')
 
 
-s = 'fvdfv45424g'
-counter = 0
-for i in range (0, 9):
-    if s.count(i) > 0:
+#s = 'fvdfv45424g'
+#counter = 0
+#for i in range (0, 9):
+#    if s.count(i) > 0:
+
+#languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+#print(languages[::-1])
+
+#numbers1 = [1, 2, 3]
+#umbers2 = [6]
+#numbers3 = [7, 8, 9, 10, 11, 12, 13]
+#print(numbers1 + numbers1 + (numbers2 * 8) + numbers3)
+
+#numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2, 12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
+#print(len(numbers))
+#print(numbers[-1])
+#print(numbers[::-1])
+#if 2 and 17 in numbers:
+#    print('YES')
+#else:
+#    print('NO')
+#del numbers[0]
+#del numbers[-1]
+#print(numbers)
+
+#Заморочиться и вспомнить перебор по алфавиту без UNICOD
+#alphabit = []
+#counter = 0
+#for i in range(97, 123):
+#    counter += 1
+#    alphabit.append(chr(i) * counter)
+#print(alphabit)
+
+#snum = []
+#x = int(input())
+#for i in range(x):
+#    num = int(input())
+#    snum.append(pow(num, 3))
+#print(snum)
+
+#alg = []
+#x = int(input())
+#for i in range(1, x + 1):
+#    if x % i == 0:
+#        alg.append(i)
+#print(alg)
+
